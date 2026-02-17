a5d7f916052f00722864c98428e288db3fffb544
philip987 <pj6118@srmist.edu.in>
2026-02-17 22:22:34 +0530

    UC3: Refactored banner using String.join()

---
+++ OOPSBannerAppUC3.java
@@ -0,0 +1,57 @@
+/**
+ * OOPSBannerApp UC3 – OOPS Banner Application (Use Case 3)
+ *
+ * This class prints the word "OOPS" using String.join()
+ * for better performance and cleaner code.
+ *
+ * @author Philip
+ * @version 3.0
+ */
+
+public class OOPSBannerAppUC3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("",
+                "  *****   ",
+                "  *****   ",
+                "  ******  ",
+                "  ******  "));
+
+        System.out.println(String.join("",
+                " **   **  ",
+                " **   **  ",
+                " **   **  ",
+                " **       "));
+
+        System.out.println(String.join("",
+                "**     ** ",
+                "**     ** ",
+                " **   **  ",
+                " **       "));
+
+        System.out.println(String.join("",
+                "**     ** ",
+                "**     ** ",
+                " ******   ",
+                " ******   "));
+
+        System.out.println(String.join("",
+                "**     ** ",
+                "**     ** ",
+                " **       ",
+                "      **  "));
+
+        System.out.println(String.join("",
+                " **   **  ",
+                " **   **  ",
+                " **       ",
+                "      **  "));
+
+        System.out.println(String.join("",
+                "  *****   ",
+                "  *****   ",
+                " **       ",
+                " ******   "));
+    }
+}
