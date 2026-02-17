9175aa79f82736f01960fb36ff13226c328638a0
philip987 <pj6118@srmist.edu.in>
2026-02-17 22:07:41 +0530

    UC2: Print OOPS Banner using print statements

---
+++ OOPSBannerAppUC2.java
@@ -0,0 +1,25 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display Application
+ *
+ * This class prints the word "OOPS" in banner format using
+ * asterisks (*) and spaces.
+ *
+ * @author Philip
+ * @version 2.0
+ */
+
+public class OOPSBannerAppUC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****     *****     ******     ****** ");
+        System.out.println(" **   **   **   **    **   **    **     ");
+        System.out.println("**     ** **     **   **   **    **     ");
+        System.out.println("**     ** **     **   ******     ****** ");
+        System.out.println("**     ** **     **   **             **");
+        System.out.println(" **   **   **   **    **             **");
+        System.out.println("  *****     *****     **         ******");
+
+    }
+
+}
